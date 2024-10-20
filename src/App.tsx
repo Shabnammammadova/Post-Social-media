63d0e53937f7f63b3fbe17501ed55f1d10558633
shabnam <shabnamjm@code.edu.az>
2024-10-20 22:15:13 +0400

    sidebar navbar and navigation some changes

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,30 @@ const App = () => {
   const [open, setOpen] = useState(true);
 
   const Navigate = [
-    { title: "Home", path: "/" },
-    { title: "Login", path: "/login" },
-    { title: "Register", path: "/register" }
+    { title: "Home", path: "/", src: "house" },
+    { title: "Login", path: "/login", src: "user-check" },
+    { title: "Register", path: "/register", src: "user-plus" }
   ];
 
   return (
     <Router>
       <div className="flex">
-        <div className={`${open ? "w-72" : "w-20"} duration-300 h-screen p-5 pt-8 bg-dark-purple relative`}>
+        <div className={`${open ? "w-72" : "w-20"} duration-300 h-screen p-5 pt-8 bg-[#49789c] relative`}>
           <img
-            src="./src/assets/control.png"
+            src="./src/assets/arrow-right.png"
             alt=""
-            className={`absolute cursor-pointer -right-3 top-9 w-7 border-2 text-dark-purple border-dark-purple rounded-full ${!open && "rotate-180"}`}
+            className={`absolute cursor-pointer -right-3 top-9 w-7 border-2 text-white border-dark-purple rounded-full ${!open && "rotate-180"}`}
             onClick={() => setOpen(!open)}
           />
           <div className="flex gap-2 items-center">
-            <img src="./src/assets/Oppologo.png" alt="" className={`cursor-pointer duration-500`} />
-            <h1 className={`text-white origin-left font-medium text-xl duration-300 ${!open && "scale-0"}`}></h1>
+            <img src="https://www.logoai.com/oss/icons/2021/10/27/l1eXTxm2w7TM6t9.png" alt="" className={`w-[40px] cursor-pointer duration-500`} />
+            <h1 className={`text-blue-900 font-extrabold origin-left font-serif  text-4xl duration-300 ${!open && "scale-0"}`}>Forbes</h1>
           </div>
-          <ul>
+          <ul className="pt-5">
             {Navigate.map((menu, index) => (
-              <li className="text-gray-300 text-sm cursor-pointer" key={index}>
-                <Link to={menu.path} className="block py-2 hover:text-white">{menu.title}</Link>
+              <li className={`flex items-center gap-x-4 *:text-blue-900 text-md font-bold cursor-pointer p-2 hover:bg-light-white rounded-md`} key={index}>
+                <img src={`./src/assets/${menu.src}.png`} alt="" />
+                <Link to={menu.path} className={`${!open && 'hidden'} origin-left duration-200`}>{menu.title}</Link>
               </li>
             ))}
           </ul>
